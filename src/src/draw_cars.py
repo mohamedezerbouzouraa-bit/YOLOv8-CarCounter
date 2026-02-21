c1b9bcb0c8b57ca16a82f0fd1af93149f125c964
Mohamed Ezer Bouzouraa <mohamedezer.bouzouraa@insat.ucar.tn>
2026-02-21 12:05:16 +0100

    Create draw_cars.py

---
+++ src/src/draw_cars.py
@@ -0,0 +1,10 @@
+import cv2
+from google.colab.patches import cv2_imshow
+
+def draw_cars(image_path, boxes):
+    img = cv2.imread(image_path)
+    for box in boxes:
+        x1, y1, x2, y2 = box
+        cv2.rectangle(img, (x1, y1), (x2, y2), (0, 255, 0), 2)
+        cv2.putText(img, "car", (x1, y1-10), cv2.FONT_HERSHEY_SIMPLEX, 0.9, (0, 255, 0), 2)
+    cv2_imshow(img)
