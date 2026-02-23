edf659ecbea0b4f92e244a22a1b01ff49df7f5f4
Mohamed Ezer Bouzouraa <mohamedezer.bouzouraa@insat.ucar.tn>
2026-02-23 19:13:03 +0100

    Drawing green boxes and labels around cars in an image.

--- src/src/draw_cars.py
+++ src/src/draw_cars.py
@@ -7,4 +7,4 @@ def draw_cars(image_path, boxes):
         x1, y1, x2, y2 = box
         cv2.rectangle(img, (x1, y1), (x2, y2), (0, 255, 0), 2)
         cv2.putText(img, "car", (x1, y1-10), cv2.FONT_HERSHEY_SIMPLEX, 0.9, (0, 255, 0), 2)
-    cv2_imshow(img)
+    cv2_imshow(img) 
