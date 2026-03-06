ed47678f06c4b0e9eedd142fd2a7cdfade480f5d
Mohamed Ezer Bouzouraa <mohamedezer.bouzouraa@insat.ucar.tn>
2026-03-06 06:49:18 +0100

    main

--- main.py
+++ main.py
@@ -4,5 +4,6 @@ from draw_cars import draw_cars
 
 image_path = upload_image()
 car_boxes = detect_cars(image_path)
+
 print("Number of cars detected:", len(car_boxes))
 draw_cars(image_path, car_boxes)
